a045949e16dbbd7a4ab80b7d28f39cecbb7a31ab
Jordan MD <bedanespiller16@gmail.com>
2026-01-11 15:25:00 +0100

    Modification de la configuration de alembic en mode Online pur le deploiment sur Railway

--- app/models/user.py
+++ app/models/user.py
@@ -17,7 +17,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(100), unique=True, nullable=False, index=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
-    hashed_password: Mapped[bool] = mapped_column(String(255), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), 
